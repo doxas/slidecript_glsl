67c14f46e84862b63baea4da021b2cc12f9fc8b9
doxas <eizanco@gmail.com>
2016-03-28 02:02:13 +0900

    input radio add

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
         var a, e;
         a = document.getElementById('content').childNodes;
         for(var i = 0, len = a.length; i < len; i++){
-            if(a[i].nodeName !== '#text' && a[i].nodeName !== '#comment'){
+            if(a[i].nodeName !== '#text'){
                 pages.push(a[i]);
             }
         }
@@ -21,7 +21,19 @@
         pageChange(true, 0);
         e = document.getElementById('total');
         e.innerText = pagesCount;
-    };
+
+        // input label setting
+        var elmList = 20;
+        var questionCount = 4;
+        for(var i = 0; i < elmList; i++){
+            var j = paddingZero(i + 1);
+            for(var k = 0; k < questionCount; k++){
+                var l = paddingZero(k + 1);
+                e = document.getElementById('radio' + j + '_' + l);
+                if(e){e.addEventListener('change', function(eve){eve.currentTarget.blur();}, true);}
+            }
+        }
+    }
 
     function keyDown(eve){
         switch(eve.keyCode){
@@ -65,6 +77,46 @@
         e.style.width = parseInt((activePage + 1) / pagesCount * 100) + '%';
         e = document.getElementById('count');
         e.innerText = activePage + 1;
+    }
+
+    // input label setting
+    function answer(){
+        var e;
+        var ans = [
+            3, 3, 4, 4, 1,
+            4, 3, 1, 2, 1
+        ];
+        var elmList = 10;
+        var ansCount = 0;
+        for(var i = 0; i < elmList; i++){
+            var j = paddingZero(i + 1);
+            var k = paddingZero(ans[i]);
+            e = document.getElementById('radio' + j + '_' + k);
+            if(e.checked){ansCount++;}
+        }
+        e = document.getElementById('ansCount');
+        e.textContent = ansCount;
+        switch(true){
+            case ansCount <= 2:
+                alert('かなり頑張って復習しないとヤバいかも！！');
+                break;
+            case ansCount <= 4:
+                alert('若干あいまいな部分が多いのかも……復習しておきましょう！');
+                break;
+            case ansCount <= 6:
+                alert('まずまず理解できているみたい。ポイントを絞って再復習！');
+                break;
+            case ansCount <= 8:
+                alert('かなり高い理解度です！　不安なところは復習しておきましょう！');
+                break;
+            default :
+                alert('perfect !!!');
+                break;
+        }
+    }
+
+    function paddingZero(num){
+        return ('0' + num).slice(-2);
     }
 })(this);
 
--- markdown/document.md
+++ markdown/document.md
@@ -52,4 +52,13 @@ paragraph
 
 --blockquote--
 
+---
+
+<input type="radio" id="radio01_01" name="radio01"><label for="radio01_01">ans 01</label>
+<input type="radio" id="radio01_02" name="radio01"><label for="radio01_02">ans 02</label>
+<input type="radio" id="radio01_03" name="radio01"><label for="radio01_03">ans 03</label>
+<input type="radio" id="radio01_04" name="radio01"><label for="radio01_04">ans 04</label>
+
+---
+
 
